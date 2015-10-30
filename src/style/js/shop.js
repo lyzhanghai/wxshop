52e670181faaaf0ef9c2ad909abce2e03288d5be
yanjinjin <jin_jin_yan@163.com>
2015-10-30 22:29:57 +0800

    update by fubiao

--- src/style/js/shop.js
+++ src/style/js/shop.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+function increase(){
+   	var num = parseInt($('#productNum').val());
+   	if(num > 1){
+   		$('#productNum').val(num-1);
+   	}
+}
+function decrease(){
+	var num = parseInt($('#productNum').val());
+    $('#productNum').val(num+1);
+}
