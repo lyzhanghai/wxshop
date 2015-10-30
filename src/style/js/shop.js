ba05c1751c5bef392367f46039312a18ec174714
yanjinjin <jin_jin_yan@163.com>
2015-10-30 22:42:04 +0800

    update by fubiao

--- src/style/js/shop.js
+++ src/style/js/shop.js
@@ -4,8 +4,18 @@ function increase(){
    	if(num > 1){
    		$('#productNum').val(num-1);
    	}
+   	settotalprice();
 }
 function decrease(){
 	var num = parseInt($('#productNum').val());
     $('#productNum').val(num+1);
+    settotalprice();
+}
+
+function settotalprice(){
+	var price = 0;
+	$('input.num').each(function(i){
+		price += parseInt($(this).attr('data')) * parseInt($(this).val())
+	})
+	$('#totalprice').html('￥' + price + '元');
 }
