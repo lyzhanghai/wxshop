a0a7af48d59169903972205e704e7328ee32fe31
yanjinjin <jin_jin_yan@163.com>
2015-10-31 12:05:42 +0800

    update by fubiao

--- src/style/js/numberInput.js
+++ src/style/js/numberInput.js
@@ -4,9 +4,11 @@ function increase(){
    	if(num > 1){
    		$('#num').val(num-1);
    	}
+   	$('#num').change();
 }
 function decrease(){
 	var num = parseInt($('#num').val());
     $('#num').val(num+1);
+    $('#num').change();
 }
 
